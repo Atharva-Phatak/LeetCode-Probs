fbea1d957128648b2859bad86839ab9fe04e7409
Atharva Phatak <athp456@gmail.com>
2021-04-12 03:47:26 +0530

    Create string_inplace_reverse.py

---
+++ String/string_inplace_reverse.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        lt , rt = 0 , len(s) - 1
+        while lt < rt:
+            s[lt] , s[rt] = s[rt] , s[lt]
+            lt , rt = lt + 1 , rt - 1
