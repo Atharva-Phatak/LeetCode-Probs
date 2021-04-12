4d0d13940d1aaa06d4f0c7722b1a0a9786bf1195
Atharva Phatak <athp456@gmail.com>
2021-04-13 00:58:57 +0530

    Update valid_palindrome.py

--- String/valid_palindrome.py
+++ String/valid_palindrome.py
@@ -5,6 +5,4 @@ class Solution:
         s = s.replace(" ","").lower()
         s = "".join([x for x in s if x not in string.punctuation])
         
-        if s == s[::-1]:
-            return True
-        return False
+        return s == s[::-1]
