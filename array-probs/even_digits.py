0444af88603c5a7fed709d879f44e201ebed1c82
Atharva Phatak <athp456@gmail.com>
2021-06-05 16:14:00 +0530

    Create even_digits.py

---
+++ array-probs/even_digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        
+        ct = 0
+        for num in nums:
+            digits = 0
+            while num > 0:
+                num //= 10
+                digits += 1
+            if digits % 2 == 0:
+                ct += 1
+        
+        return ct
